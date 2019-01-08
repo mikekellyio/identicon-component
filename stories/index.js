03c977db5e98763534bb92fcc4a7771100dfb07a
Mike Kelly <mike@kellyfam.net>
2019-01-08 08:48:49 -0500

    add hash specified story

--- stories/index.js
+++ stories/index.js
@@ -2,4 +2,8 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import Identicon from "../src/";
 
-storiesOf("Identicon", module).add("defaults", () => <Identicon />);
+storiesOf("Identicon", module)
+  .add("defaults", () => <Identicon />)
+  .add("with hash specified", () => (
+    <Identicon hash={"47419ab5e63b2ed07767a5208c1bd658"} />
+  ));
